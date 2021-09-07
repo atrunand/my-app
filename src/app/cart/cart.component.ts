a2d49eee318769fa92c3443a70c16e2be01dcead
atrunand <aticha.ruengwattana@mail.kmutt.ac.th>
2021-09-07 19:41:52 +0700

    7-9-end

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 import { CartService } from "../cart.service";
 
+import { FormBuilder } from "@angular/forms";
+
 @Component ({
   selector: 'app-cart', //ตัวเลือกองค์ประกอบ CSS ของส่วนประกอบ
   templateUrl: './cart.component.html', //ตำแหน่งของไฟล์เทมเพลตของส่วนประกอบ
@@ -11,10 +13,26 @@ import { CartService } from "../cart.service";
 export class CartComponent implements OnInit {
 
   items: any;
+  checkoutForm: any;
 
   constructor(
-    private cartService: CartService
-  ) {}
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData: any) {
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset(); //resrt form
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
